97d42a11cafb55aed4569c43ede39a392ac0bad3
kirill_k <kirill.kot.1989@gmail.com>
2017-11-19 03:52:13 +0300

    Added Angular Material elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { MatToolbarModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { HeaderModule } from './header/header.module';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HeaderModule,
+    MatToolbarModule,
     AppRoutingModule
   ],
   providers: [],
